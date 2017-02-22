e71fda23216145fc5c0a6d9a8bc9dae50b81459b
emptyewer <venky.krishna@icloud.com>
2017-02-22 14:00:08 -0600

    changed importnames

--- R/importFromDeepn.R
+++ R/importFromDeepn.R
@@ -6,10 +6,10 @@ importFromDeepn <- function(file) {
   ind <- order(key)
   key <- key[ind]
   val <- val[ind]
-  vn <- val[grep("Vector_Non", key)]
-  vs <- val[grep("Vector_Sel", key)]
-  bn <- val[grep("Bait._Non", key)]
-  bs <- val[grep("Bait._Sel", key)]
+  vn <- val[grep("_nonsel_vector", key)]
+  vs <- val[grep("_sel_vector", key)]
+  bn <- val[grep("_nonsel_bait.", key)]
+  bs <- val[grep("_sel_bait.", key)]
   Data <- import(vn, vs, bn, bs)
   applyFilter(Data, as.numeric(val[key=="Threshold"]))
 }
