acf7a11b12db5fa435458c6551396b41f55d10d0
Venkatramanan Krishnamani <venky.agas@gmail.com>
2017-03-08 16:53:20 -0600

    Update analyzeDeepn.R

--- R/analyzeDeepn.R
+++ R/analyzeDeepn.R
@@ -1,6 +1,11 @@
 analyzeDeepn <- function(infile, outfile="stat.csv", debug=FALSE, sort=1, port) {
   Data <- importFromDeepn(infile)
   Data$omega <- overdisp(Data)
+  print("Overdispersion estimates", paste("Baseline (vector only):   ", formatC(Data$omega["Baseline"], digits=2, format="f")), 
+        paste("Baseline (vector + bait): ", formatC(Data$omega["baitEffect"], digits=2, format="f")),
+        paste("Selection:                ", formatC(Data$omega["Selected"], digits=2, format="f")),
+        "-----", "Baseline overdispersions should be around 0 (no overdispersion)", 
+        "Overdispersion under selection conditions will ideally be around 1 or lower")
   if (!missing(port)) {
     con <- socketConnection(host="localhost", port=port)
     out <- c("Overdispersion estimates",
